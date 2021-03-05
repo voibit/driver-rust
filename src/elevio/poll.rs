4279f4720bbd6b7928803a30125bd474e5107952
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2021-03-05 15:26:18 +0100

    Run rustfmt and fix some Clippy warnings (#1)
    * Run rustfmt
    
    * Fix some clippy warnings

--- src/elevio/poll.rs
+++ src/elevio/poll.rs
@@ -1,26 +1,23 @@
-
-
-use std::time;
-use std::thread;
 use crossbeam_channel as cbc;
+use std::thread;
+use std::time;
 
 use super::elev;
 
 #[derive(Debug)]
 pub struct CallButton {
-    pub floor:  u8,
-    pub call:   u8,
+    pub floor: u8,
+    pub call: u8,
 }
 
-pub fn call_buttons(elev: elev::Elevator, ch: cbc::Sender<CallButton>, period: time::Duration){
-
+pub fn call_buttons(elev: elev::Elevator, ch: cbc::Sender<CallButton>, period: time::Duration) {
     let mut prev = vec![[false; 3]; elev.num_floors.into()];
     loop {
         for f in 0..elev.num_floors {
             for c in 0..3 {
                 let v = elev.call_button(f, c);
-                if v  &&  prev[f as usize][c as usize] != v {
-                    ch.send(CallButton{floor: f, call: c}).unwrap();
+                if v && prev[f as usize][c as usize] != v {
+                    ch.send(CallButton { floor: f, call: c }).unwrap();
                 }
                 prev[f as usize][c as usize] = v;
             }
@@ -29,24 +26,20 @@ pub fn call_buttons(elev: elev::Elevator, ch: cbc::Sender<CallButton>, period: t
     }
 }
 
-pub fn floor_sensor(elev: elev::Elevator, ch: cbc::Sender<u8>, period: time::Duration){
-    
+pub fn floor_sensor(elev: elev::Elevator, ch: cbc::Sender<u8>, period: time::Duration) {
     let mut prev = u8::MAX;
     loop {
-        match elev.floor_sensor() {
-            Some(f) => 
-                if f != prev {
-                    ch.send(f).unwrap();
-                    prev = f;
-                },
-            None => (),
+        if let Some(f) = elev.floor_sensor() {
+            if f != prev {
+                ch.send(f).unwrap();
+                prev = f;
+            }
         }
         thread::sleep(period)
     }
 }
 
-pub fn stop_button(elev: elev::Elevator, ch: cbc::Sender<bool>, period: time::Duration){
-    
+pub fn stop_button(elev: elev::Elevator, ch: cbc::Sender<bool>, period: time::Duration) {
     let mut prev = false;
     loop {
         let v = elev.stop_button();
@@ -58,8 +51,7 @@ pub fn stop_button(elev: elev::Elevator, ch: cbc::Sender<bool>, period: time::Du
     }
 }
 
-pub fn obstruction(elev: elev::Elevator, ch: cbc::Sender<bool>, period: time::Duration){
-    
+pub fn obstruction(elev: elev::Elevator, ch: cbc::Sender<bool>, period: time::Duration) {
     let mut prev = false;
     loop {
         let v = elev.obstruction();
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use std::thread::*;
 use std::time::*;
 
@@ -11,52 +10,41 @@ mod elevio {
 use elevio::elev as e;
 
 fn main() -> std::io::Result<()> {
-    
     let elev_num_floors = 4;
     let elevator = e::Elevator::init("localhost:15657", elev_num_floors)?;
-    println!("Elevator started:\n{:#?}", elevator);    
+    println!("Elevator started:\n{:#?}", elevator);
 
-    
     let poll_period = Duration::from_millis(25);
-    
+
     let (call_button_tx, call_button_rx) = cbc::unbounded::<elevio::poll::CallButton>();
     {
         let elevator = elevator.clone();
-        spawn(move ||{
-            elevio::poll::call_buttons(elevator, call_button_tx, poll_period) 
-        });
+        spawn(move || elevio::poll::call_buttons(elevator, call_button_tx, poll_period));
     }
-    
-    let (floor_sensor_tx, floor_sensor_rx) = cbc::unbounded::<u8>();    
+
+    let (floor_sensor_tx, floor_sensor_rx) = cbc::unbounded::<u8>();
     {
         let elevator = elevator.clone();
-        spawn(move ||{
-            elevio::poll::floor_sensor(elevator, floor_sensor_tx, poll_period)
-        });
+        spawn(move || elevio::poll::floor_sensor(elevator, floor_sensor_tx, poll_period));
     }
-    
-    let (stop_button_tx, stop_button_rx) = cbc::unbounded::<bool>();    
+
+    let (stop_button_tx, stop_button_rx) = cbc::unbounded::<bool>();
     {
         let elevator = elevator.clone();
-        spawn(move ||{
-            elevio::poll::stop_button(elevator, stop_button_tx, poll_period)
-        });
+        spawn(move || elevio::poll::stop_button(elevator, stop_button_tx, poll_period));
     }
-    
-    let (obstruction_tx, obstruction_rx) = cbc::unbounded::<bool>();    
+
+    let (obstruction_tx, obstruction_rx) = cbc::unbounded::<bool>();
     {
         let elevator = elevator.clone();
-        spawn(move ||{
-            elevio::poll::obstruction(elevator, obstruction_tx, poll_period)
-        });
+        spawn(move || elevio::poll::obstruction(elevator, obstruction_tx, poll_period));
     }
-    
-    
+
     let mut dirn = e::DIRN_DOWN;
     if elevator.floor_sensor().is_none() {
         elevator.motor_direction(dirn);
     }
-    
+
     loop {
         cbc::select! {
             recv(call_button_rx) -> a => {
@@ -67,7 +55,7 @@ fn main() -> std::io::Result<()> {
             recv(floor_sensor_rx) -> a => {
                 let floor = a.unwrap();
                 println!("Floor: {:#?}", floor);
-                dirn = 
+                dirn =
                     if floor == 0 {
                         e::DIRN_UP
                     } else if floor == elev_num_floors-1 {
